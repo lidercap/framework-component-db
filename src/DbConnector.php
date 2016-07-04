ccb1c99b32b2263258f89b28c3cc5b6179a8b1c3
Leonardo Thibes <leonardothibes@gmail.com>
2016-07-04 13:58:00 -0300

    testes

--- src/DbConnector.php
+++ src/DbConnector.php
@@ -2,15 +2,44 @@
 
 namespace Lidercap\Component\Db;
 
+/**
+ * Conector de banco de dados.
+ */
 class DbConnector implements DbConnectorInterface
 {
     use DbConfigAware;
 
     /**
+     * @var array
+     */
+    protected $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_CASE               => \PDO::CASE_LOWER,
+    ];
+
+    /**
+     * @return string
+     */
+    public function getDsn()
+    {
+        return sprintf('dblib:host=%s;dbname=%s', $this->hostname, $this->database);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     *
      * @return \PDO
      */
     public function getConnection()
     {
+        $dsn = $this->getDsn();
 
+        $db = new \PDO($dsn, $this->username, $this->password, $this->options);
+        $db->exec('SET ANSI_PADDING ON');
+        $db->exec('SET ANSI_WARNINGS ON');
+        $db->exec('SET CONCAT_NULL_YIELDS_NULL ON');
+
+        return $db;
     }
 }
--- tests/DbConnectorTest.php
+++ tests/DbConnectorTest.php
@@ -11,4 +11,19 @@ class DbConnectorTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf(DbConnectorInterface::class, new DbConnector);
     }
+
+    public function testGetDsn()
+    {
+        $hostname = 'hostname-' . rand(1, 100);
+        $database = 'database-' . rand(1, 100);
+
+        $dbConnector = new DbConnector();
+        $dbConnector->setHostname($hostname);
+        $dbConnector->setDatabase($database);
+
+        $this->assertEquals(
+            'dblib:host='. $hostname .';dbname=' . $database,
+            $dbConnector->getDsn()
+        );
+    }
 }
