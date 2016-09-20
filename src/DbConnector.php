86ef6403304aec278cd1381619cecb3dbdf13fdf
Leonardo Thibes <leonardothibes@gmail.com>
2016-09-20 11:55:41 -0300

    alterando parametros do driver para converter retornos de campos em branco em queryes para string em branco ao invés de NULL

--- src/DbConnector.php
+++ src/DbConnector.php
@@ -16,6 +16,7 @@ class DbConnector implements DbConnectorInterface
         \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_CASE               => \PDO::CASE_LOWER,
+        \PDO::ATTR_ORACLE_NULLS       => \PDO::NULL_TO_STRING,
     ];
 
     /**
